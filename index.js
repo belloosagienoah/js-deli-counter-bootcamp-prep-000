37de4317460003b72281cf11b89c89f79c697654
Bello Osagie <osagienoah04@gmail.com>
2019-07-08 19:28:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var takeANumber = function(katzDeli, newCustomer) {
      return ('Welcome, ' + newCustomer + '. You are number ' + katzDeli.length + ' in line.')
   }
 
-  // function nowServing (katzDeliLine) { //declared function
-  //    if(katzDeliLine.length===0) { //checks if length of array(katzDeli) is = to 0
-  //    return 'There is nobody waiting to be served!'; //returns string no one is waiting
-  // }
-  //    var customer = katzDeliLine.shift(); // shift array of people
-  //    return "Currently serving " + customer + "."; //return string with customer name
-  // }
+  function nowServing (katzDeliLine) { //declared function
+     if(katzDeliLine.length===0) { //checks if length of array(katzDeli) is = to 0
+     return 'There is nobody waiting to be served!'; //returns string no one is waiting
+  }
+     var customer = katzDeliLine.shift(); // shift array of people
+     return "Currently serving " + customer + "."; //return string with customer name
+  }
