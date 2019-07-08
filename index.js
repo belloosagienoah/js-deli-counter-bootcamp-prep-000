e75b562ed32a076e3408750d1636205a890c9cce
Bello Osagie <osagienoah04@gmail.com>
2019-07-08 19:27:29 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,6 @@
+// var takeANumber = function(katzDeli, newCustomer) {
+
+//     if (katzDeli.length > 0);
+//      katzDeli.push(newCustomer);
+//      return ('Welcome, ' + newCustomer + '. You are number ' + katzDeli.length + ' in line.')
+//   }
