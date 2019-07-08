987888d8be7536629ce233cebbdc2be766536807
Bello Osagie <osagienoah04@gmail.com>
2019-07-08 19:20:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function takeANumber (katzDeli, newCustomer){
 
 takeANumber(['me'], "sonia")
 
-// Build a function currentLine that returns the current line. For example, if katzDeliLine is currently ["Ada", "Grace"], currentLine(katzDeliLine) would return "The line is currently: 1. Ada, 2. Grace". If there is nobody in line, it should return "The line is currently empty."
 
 function currentLine(katzDeli){
   if (katzDeli.length===0){
